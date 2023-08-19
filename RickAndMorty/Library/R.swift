255d0fd0c05e3ef63261c0d84f993169042cdda1
Tsimintiya Nikolay <wwwwat@inbox.ru>
2023-08-19 05:53:43 +0300

    Fonts added

--- RickAndMorty/Library/R.swift
+++ RickAndMorty/Library/R.swift
@@ -5,11 +5,21 @@
 //  Created by Николай Циминтия on 18.08.2023.
 //
 
-import Foundation
+import UIKit
 
 struct R {
     public struct Fonts {
+        public static let gilroyBold = "Gilroy-Bold"
         public static let gilroySemibold = "Gilroy-Semibold"
         public static let gilroyMedium = "Gilroy-Medium"
+    }
+    
+    public struct Colors {
+        public static let baseBackground = UIColor(red: 4/255, green: 10/255, blue: 25/255, alpha: 1)
+        public static let cellBackground = UIColor(red: 0.15, green: 0.163, blue: 0.221, alpha: 1)
+        public static let greenLabel = UIColor(red: 0.278, green: 0.775, blue: 0.045, alpha: 1)
+        public static let lightGrayLabel = UIColor(red: 0.768, green: 0.789, blue: 0.808, alpha: 1)
+        public static let dateLabel = UIColor(red: 0.577, green: 0.595, blue: 0.613, alpha: 1)
+        public static let planetBackground = UIColor(red: 0.098, green: 0.11, blue: 0.165, alpha: 1)
     }
 }
